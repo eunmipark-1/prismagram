42fe076104a1b04cadf87379cf02684180ba8104
박은미 <mia1221@kakao.com>
2020-10-15 13:35:22 +0900

    내정보 return 수정

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -1,21 +1,26 @@
+import { prisma } from "../../../../generated/prisma-client";
 
 export default {
   Mutation : {
     createAccount: async(_, args) => {
-      const {userName, email, firstName="", lastname="",bio=""} = args;
-      try {
-        const user = await prisma.createUser({
-          userName,
-          email,
-          firstName,
-          lastname,
-          bio   
-        });
-        return true;
-      } catch {
-        return false;
+      const {userName, email, firstName="", lastname="",bio=""} = args;     
+      const exists = await prisma.$exists.user({
+        OR: [
+          {userName},
+          {email}
+        ]
+      });
+      if(exists){
+        throw Error("This username/ email  is aleady taken");
       }
-
+      await prisma.createUser({
+        userName,
+        email,
+        firstName,
+        lastname,
+        bio   
+      });
+      return true;
     }
   }
 };
